09d0ce9c5ba14c2a02b5ea811beff86e9d6505c9
Nolan-James <nj@nolanjames.me>
2024-04-27 18:18:46 +0100

    Initial Commit

---
+++ book-network-api/src/main/java/me/nolanjames/booknetworkapi/BookNetworkApiApplication.java
@@ -0,0 +1,13 @@
+package me.nolanjames.booknetworkapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookNetworkApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookNetworkApiApplication.class, args);
+    }
+
+}
